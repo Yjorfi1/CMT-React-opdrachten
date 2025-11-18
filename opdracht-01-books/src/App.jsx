85c55ffba110ff2a49c9f3b60098b02528f00247
yjorfi1 <yjorfi@hotmail.com>
2025-11-18 11:17:44 +0100

    .

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -20,8 +20,6 @@ function App() {
     <Route path="/about" element={<About/>}/>
      <Route path="/home" element={<Home />}/>
     <Route path="/contact" element={<Contact />}/> 
-    <Route path='*' element={<NoPage />}/>
-
 </Route>
     
    </Routes>
