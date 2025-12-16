8e0db173b620d123c8bc8cb33383d13d0573f08b
yjorfi1 <yjorfi@hotmail.com>
2025-12-16 11:51:57 +0100

    opdracht 13 af

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -5,39 +5,48 @@ import { useState } from "react";
 
 function Book({ title, author, category, image, desc, releaseyear, pages }) {
   const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
-  const [flipped, setFlipped] = useState(true);
+    const [liked, setLiked] = useState(false);
+  const [Flipped, setFlipped] = useState(true);
+
+  const toggleLike = (e) =>{
+    e.stopPropagation()
+    setLiked(!liked)};
+  
 
   const verhoogTeller = () => {
     setAantalKeerGelezen(aantalKeerGelezen + 1);
   };
 
   const flippHandler = () => { 
-    setFlipped(!flipped)
+    setFlipped(!Flipped)
    }
-
 
 return (
   <div className="books">
     <section onClick={flippHandler} className="book-container">
-      {flipped ?
+      {Flipped ?
         <div className="book-front">
           <img id="image" src={image} alt={title} />
           <h2>{title}</h2>
           <p>{author}</p>
           <p>{category}</p>
+          <button onClick={toggleLike}>{liked ? "❤️" : "👹"}</button>
+                 {liked && (<p className="favorite-text">Toegevoegd</p>)} 
+
         </div>
         :
         <div className="book-back">
-                    <img id="image" src={image} alt={title} />
-          <p>{desc}</p>
-          <h3>{releaseyear}</h3>
-          <h3>{pages}</h3>
+          <img id="image" src={image} alt={title} />
+          <p>Beschrijving: {desc}</p>
+          <h3>Jaar: {releaseyear}</h3>
+          <h3>Pagina's: {pages}</h3>
           
         </div>
       }
     </section> 
     </div>
 )
+
 }
 
 
